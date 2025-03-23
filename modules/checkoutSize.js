7f7277db4ef1d7c530164791c7f1e916e926f29a
Alexandru Bara <alexandrubara2000@gmail.com>
2025-03-23 19:45:47 +0000

    Made experiment run with taint analysis

--- modules/checkoutSize.js
+++ modules/checkoutSize.js
@@ -1,17 +1,55 @@
+const fs = require("fs");
 const common = rootRequire("core/common");
-const browser = rootRequire("core/browser");
 const crawler = rootRequire("core/crawler");
 const importer = rootRequire("core/importer");
 const db = rootRequire("core/db");
-const fs = require("fs");
+const browser = rootRequire("core/browser");
+const http = require('http');
+const downloadPath = process.env.CRAWLER_DOWNLOAD_PATH || "/tmp";
 
 const event = common.event;
 const options = {
-    browser: {args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-extensions-except=consent-o-matic',
-        '--load-extension=consent-o-matic',]},
-    context: {},
+    browser: {
+        firefoxUserPrefs: {
+            "browser.sessionstore.resume_from_crash": false,
+            "browser.tabs.crashReporting.sendReport": false,
+            "media.autoplay.default": 5,
+            "media.autoplay.allow-extension-background-pages": false,
+            "media.autoplay.block-event.enabled": true,
+            "media.autoplay.enabled.user-gestures-needed": false,
+            "dom.always_stop_slow_scripts": true,
+            "dom.use_watchdog": true,
+            "dom.max_script_run_time": 30,
+            "dom.max_chrome_script_run_time": 60,
+            "dom.max_ext_content_script_run_time": 15,
+            "browser.cache.disk.enable": false,
+            "browser.cache.memory.enable": false,
+            "privacy.trackingprotection.enabled": false,
+            "privacy.trackingprotection.fingerprinting.enabled": false,
+            "privacy.trackingprotection.origin_telemetry.enabled": false,
+            "privacy.trackingprotection.socialtracking.enabled": false,
+            "privacy.trackingprotection.pbmode.enabled": false,
+            "privacy.socialtracking.block_cookies.enabled": false,
+            "network.cookie.cookieBehavior": 0,
+            "security.fileuri.strict_origin_policy": false,
+            "browser.download.folderList": 2,
+            "pdfjs.disabled": true ,
+            "browser.download.manager.showWhenStarting": false,
+            "browser.download.dir": downloadPath,
+            "browser.helperApps.neverAsk.saveToDisk": "text/csv,application/x-msexcel,application/excel,application/x-excel,application/vnd.ms-excel,image/png,image/jpeg,text/html,text/plain,application/msword,application/xml,application/pdf,application/zip",
+            "browser.helperApps.alwaysAsk.force": false,
+            "browser.download.manager.alertOnEXEOpen": false,
+            "browser.download.manager.focusWhenStarting": false,
+            "browser.download.manager.useWindow": false,
+            "browser.download.manager.showAlertOnComplete": false,
+            "browser.download.manager.closeWhenDone": false
+        }
+    }, context: {
+        ignoreHTTPSErrors: true,
+        hasTouch: true
+    },
     crawler: {maxDepth: 3, maxLinks: 10, randomizeLinks: true, maxRetries: 2, sameSite: false, depthFirst: true,},
-    seed: {list: "top118Canada.csv", pageLimit: 100000},
+    seed: {list: "refined_118_list.csv", pageLimit: 100000},
 };
 
 module.exports = {
@@ -35,11 +73,29 @@ async function seed() {
 }
 
 //Called every time the browser context is restarted
+const flowHandler = common.readFile("snippets/flowHandler.js");
+let findings = [];
+let checkoutWasFound=false;
+
 async function initialize() {
 }
 
 //Before visiting a new page from the crawling queue
 async function before(params) {
+    await browser.context().addInitScript(flowHandler);
+    browser.context().exposeBinding("__crawler_taint_report", async function (source, value) {
+	console.log("Made it to 1");
+        if (checkoutWasFound){
+	    console.log("Made it to 2");
+            let finding = await enhance_finding(value);
+            // await send_finding(params, Object.assign({errored: params.error !== undefined }, finding));
+            if (finding.taint){
+                console.log(finding);
+                await send_finding(params, Object.assign({errored: params.error !== undefined }, finding));
+            }
+            // findings.push(value);
+        }
+    });
 }
 
 //During the visit, after the page has loaded
@@ -63,6 +119,260 @@ async function during(params) {
 
 //After the page was closed, useful for postprocessing and DB operations
 async function after(params) {
+    // for (let f of findings) {
+    //     let finding = await enhance_finding(f);
+    //     // await send_finding(params, Object.assign({errored: params.error !== undefined }, finding));
+    //     if (finding.taint){
+    //         // console.log(finding);
+    //         await send_finding(params, Object.assign({errored: params.error !== undefined }, finding));
+    //     }
+    // }
+    // console.log(`Exported ${findings.length} findings for ${params.protocol + params.host}`);
+}
+
+async function enhance_finding(finding) {
+    let taints = [];
+    if (!finding.taint){
+        return finding;
+    }
+    for(let taint of finding.taint) {
+        let ops = [];
+        for(let op of taint.flow) {
+            ops.push({operation: op.operation, source: op.source, builtin: op.builtin, function: op.location.function});
+        }
+        taint.hash = common.hash(ops);
+        taints.push(taint);
+    }
+    finding.taint = taints;
+    return finding;
+}
+
+async function send_finding(params, finding) {
+    if (!finding.taint){
+        return;
+    }
+    const url = `${params.protocol}${params.host}${params.path}${params.query}${params.fragment}`;
+    const data = JSON.stringify({finding: Object.assign({pid: params.pid, base_url: url}, finding)});
+    const options = {
+        hostname: '127.0.0.1',
+        port: 3000,
+        path: '/finding',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': Buffer.byteLength(data)
+        },
+    };
+
+    const req = http.request(options, res => {
+        // console.log(`statusCode: ${res.statusCode}`);
+
+        // res.on('data', d => {
+        // process.stdout.write(d);
+        // });
+    });
+
+    req.on('error', error => {
+        console.error(`Error sending finding to export service: ${error} -- TERMINATING`);
+        process.exit(5);
+    });
+
+    req.write(data);
+    req.end();
+
+}
+
+const triggerClickEvent = async (page) => {
+    await page.evaluate(() => {
+        document.body.click();
+    });
+    await common.sleep(500);
+    await page.evaluate(() => {
+        document.body.click();
+    });
+    await common.sleep(500);
+    await page.evaluate(() => {
+        document.body.click();
+    });
+    await common.sleep(500);
+    await page.evaluate(() => {
+        document.body.click();
+    });
+}
+
+const triggerFocusBlurEvent = async (page) => {
+    const inputElements = await page.$$('input');
+    for (const input of inputElements) {
+        try {
+      // Scroll the element into view
+      await page.evaluate((element) => {
+        element.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'center' });
+      }, input);
+
+      // Wait for the element to be visible and clickable
+      await page.waitForSelector('input', { visible: true, timeout: 1500});
+      // Click the element
+      await input.click({timeout:0});
+      console.log('Clicked input element');
+      // Optionally wait a bit between clicks
+    //   await page.waitForTimeout(500);
+    } catch (error) {
+      console.error('Error clicking input element:', error);
+    }
+  }
+    //To trigger blur event
+    // await page.click('body')
+}
+
+const triggerDoubleClickEvent = async(page) => {
+    await page.evaluate(() => {
+        const element = document.querySelector('body'); // Replace 'body' with any valid selector for the element you want to double-click.
+        if (element) {
+            const event = new MouseEvent('dblclick', {
+                bubbles: true,
+                cancelable: true,
+                view: window
+            });
+            console.log('Attempting to trigger double click event handlers')
+            element.dispatchEvent(event);
+        }
+    });
+    
+}
+const triggerMouseEvents = async (page) => {
+    // Simulate other mouse events on the first input
+    try {
+        const body = await page.$$('body');
+    
+        const box = await body[0].boundingBox();
+
+        // Mouse move to the element
+        await page.mouse.move(box.x + box.width / 2, box.y + box.height / 2, {timeout: 60000});
+
+        // Mouse down event
+        await page.mouse.down({timeout: 60000});
+
+        // Mouse up event
+        await page.mouse.up({timeout: 60000});
+
+        // Mouse enter event doesn't directly exist, but moving the mouse to the element simulates it
+        await page.mouse.move(box.x + box.width / 2, box.y + box.height / 2, {timeout: 60000});
+    } catch (e) {
+        console.log('Error occured while trying to trigger mousemove event: ' + e);
+    }
+    
+}
+
+
+const triggerKeyEvents = async (page) => {
+    await page.keyboard.press('Tab', { delay: 100 });
+    await page.keyboard.down('Shift');
+    await page.keyboard.press('!');  // Example to show Shift+1 -> !
+    await page.keyboard.up('Shift');
+}
+const triggerCopyPasteEvents = async (page) => {
+    await page.keyboard.down('Control');
+    await page.keyboard.press('C'); // Assuming Windows/Linux. Use 'Meta' for macOS.
+    await page.keyboard.up('Control');
+
+    await page.keyboard.down('Control');
+    await page.keyboard.press('V'); // Assuming Windows/Linux. Use 'Meta' for macOS.
+    await page.keyboard.up('Control');
+}
+
+const triggerScrollEvent = async (page) => {
+    const scrollStep = 100; // 100 px per step
+    const scrollInterval = 100; // ms between each scroll
+    let lastPosition = 0;
+    let newPosition = 0;
+
+    while (true) {
+        // console.log("1")
+        newPosition = await page.evaluate((step) => {
+            window.scrollBy(0, step);
+            return window.pageYOffset;  // Get the new scroll position
+        }, scrollStep);
+        // If no more scrolling is possible, break the loop
+        if (newPosition === lastPosition) {
+            break;
+        }
+        lastPosition = newPosition;
+        await common.sleep(scrollInterval);  // Wait before the next scroll
+    }
+
+    // Optionally scroll up or down using mouse if necessary
+    try {
+        await page.mouse.wheel(0, -100);  // Ensure enough timeout if mouse interaction is needed
+    } catch (error) {
+        console.error("Mouse wheel error:", error);
+    }
+}
+
+const triggerWindowResize = async (page) => {
+   
+    const landscape = { width: 1280, height: 1000 };
+    await page.setViewportSize(landscape);
+    console.log('Set to landscape');
+
+}
+const triggerOrientationChangeEvents = async (page) => {
+    // Dispatch an orientation change event
+    await page.evaluate(() => {
+      // Simulate changing to landscape
+      Object.defineProperty(screen, 'orientation', {
+          value: { angle: 90, type: 'landscape-primary' },
+          writable: true
+      });
+
+      // Create and dispatch the event
+      const event = new Event('orientationchange');
+      window.dispatchEvent(event);
+  });
+
+}
+
+
+const triggerTouchEvents = async (page) => {
+    try{
+        await page.touchscreen.tap(100, 150);
+        await common.sleep(500);
+        await page.touchscreen.tap(120, 130);
+        await common.sleep(500);
+        await page.touchscreen.tap(140, 110);
+    }
+    catch (error) {
+        console.error("touch event error error:", error);
+    }
+}
+
+const triggerEventHandlers = async (page) => {
+    await common.sleep(250);
+    console.log('Triggering the click event')
+    await triggerClickEvent(page)
+    await common.sleep(250);
+    console.log('Triggering double click event')
+    await triggerDoubleClickEvent(page)
+    await common.sleep(250);
+    console.log('Triggering mouse events')
+    await triggerMouseEvents(page)
+    await common.sleep(250);
+    console.log('Triggering keyboard events')
+    await triggerKeyEvents(page)
+    await common.sleep(250);
+    console.log('Triggering copy/paste events')
+    await triggerCopyPasteEvents(page)
+    await common.sleep(250);
+    console.log('Triggering scroll/wheel events')
+    await triggerScrollEvent(page)
+    await common.sleep(250);
+    console.log('Triggering resize events')
+    await triggerWindowResize(page)
+    await common.sleep(250);
+    console.log('Triggering orientation events')
+    await triggerOrientationChangeEvents(page)
+    await common.sleep(250);
+    console.log('Triggering touch events')
+    await triggerTouchEvents(page)
 }
 
 
@@ -347,6 +657,8 @@ async function goToCheckout(page, params, depth){
             let screenshot = await browser.page().screenshot();
             fs.writeFileSync(`screenshots/checkout/size/${params.pid}-${params.host}-${i}.png`, Buffer.from(screenshot, "base64"));
             await markAsDone(params);
+            checkoutWasFound=true;
+            await triggerEventHandlers(page);
             if (depth == 0){
                 await goToCheckout(page, params,1);
             }
